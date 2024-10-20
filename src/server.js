a94e390ac39db0b7dc8f3daf6ac873cb4940ac72
smeraldo1306 <trongphucngo239@gmail.com>
2024-10-20 10:05:01 +0700

    Initialize server base

---
+++ src/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import { env } from '~/config/environment';
+import { StatusCodes } from 'http-status-codes';
+
+const START_SERVER = () => {
+   const app = express();
+
+   app.use(cors());
+
+   app.use(express.json());
+
+   app.use('/', (req, res) => {
+      res.status(StatusCodes.OK).json({
+         status: 'Success',
+         message: 'Welcome to our application.',
+      });
+   });
+
+   app.listen(env.APP_PORT, env.APP_HOST, () => {
+      console.log(
+         `Server is running at http://${env.APP_HOST}:${env.APP_PORT}/`
+      );
+   });
+};
+
+try {
+   START_SERVER();
+} catch (error) {
+   console.log('Cannot connect to server!', error);
+   process.exit(0);
+}
