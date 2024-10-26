dbffcec45e585c81eb1f5b9507fcaaf153d26a41
SoulMinT05 <minhtam5111993@gmail.com>
2024-10-26 21:31:03 +0700

    first commit

--- src/server.js
+++ src/server.js
@@ -4,29 +4,27 @@ import { env } from '~/config/environment';
 import { StatusCodes } from 'http-status-codes';
 
 const START_SERVER = () => {
-   const app = express();
+    const app = express();
 
-   app.use(cors());
+    app.use(cors());
 
-   app.use(express.json());
+    app.use(express.json());
 
-   app.use('/', (req, res) => {
-      res.status(StatusCodes.OK).json({
-         status: 'Success',
-         message: 'Welcome to our application.',
-      });
-   });
+    app.use('/', (req, res) => {
+        res.status(StatusCodes.OK).json({
+            status: 'Success',
+            message: 'Welcome to our application.',
+        });
+    });
 
-   app.listen(env.APP_PORT, env.APP_HOST, () => {
-      console.log(
-         `Pharamacy Server is running at http://${env.APP_HOST}:${env.APP_PORT}/`
-      );
-   });
-}
+    app.listen(env.APP_PORT, env.APP_HOST, () => {
+        console.log(`Pharamacy Server is running at http://${env.APP_HOST}:${env.APP_PORT}/`);
+    });
+};
 
 try {
-   START_SERVER();
+    START_SERVER();
 } catch (error) {
-   console.log('Cannot connect to server!', error);
-   process.exit(0);
+    console.log('Cannot connect to server!', error);
+    process.exit(0);
 }
