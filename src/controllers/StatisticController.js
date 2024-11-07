7c5aebc5c293cc9ba5849fadfdf7244d555537b2
SoulMinT05 <minhtam5111993@gmail.com>
2024-11-07 20:10:49 +0700

    completed statistic by day

--- src/controllers/StatisticController.js
+++ src/controllers/StatisticController.js
@@ -10,13 +10,14 @@ const statisticDay = asyncHandler(async (req, res) => {
         return res.status(400).json({ error: 'startDate and endDate are required' });
     }
 
+    // Phiếu nhập
     // Câu truy vấn SQL để lấy thống kê hóa đơn nhận hàng trong khoảng thời gian
-    const query = `
+    const queryListInput = `
     SELECT 
       rn.received_note_id,
       rn.employee_id,
       rn.supplier_id,
-      rn.received_date,
+      CONVERT_TZ(rn.received_date, '+00:00', '+07:00') AS received_date,
       rnd.medicine_id,
       rnd.quantity,
       rnd.price
@@ -28,13 +29,57 @@ const statisticDay = asyncHandler(async (req, res) => {
       DATE(rn.received_date) BETWEEN ? AND ?;
   `;
 
-    const [results] = await connection.promise().query(query, [startDate, endDate]);
-    console.log('results: ', results);
+    const [resultsInput] = await connection.promise().query(queryListInput, [startDate, endDate]);
+
+    const maxPriceRowInput = resultsInput.reduce((max, row) => (row.price > max.price ? row : max), resultsInput[0]);
+    const minPriceRowInput = resultsInput.reduce((min, row) => (row.price < min.price ? row : min), resultsInput[0]);
+
+    const totalPriceInput = resultsInput.reduce((sum, row) => sum + row.price, 0);
+    const avgPriceInput = totalPriceInput / resultsInput.length;
+
+    // Phiếu chi
+    // Câu truy vấn SQL để lấy thống kê hóa đơn nhận hàng trong khoảng thời gian
+    const queryListOutput = `
+    SELECT 
+      dv.delivery_note_id,
+      dv.employee_id,
+      dv.customer_id,
+      CONVERT_TZ(dv.delivery_date, '+00:00', '+07:00') AS delivery_date,
+      dvd.medicine_id,
+      dvd.quantity,
+      dvd.price
+    FROM 
+      DeliveryNotes dv
+    JOIN 
+      DeliveryNoteDetails dvd ON dv.delivery_note_id = dvd.delivery_note_id
+    WHERE 
+      DATE(dv.delivery_date) BETWEEN ? AND ?;
+  `;
+
+    const [resultsOutput] = await connection.promise().query(queryListOutput, [startDate, endDate]);
+
+    const maxPriceRowOutput = resultsOutput.reduce((max, row) => (row.price > max.price ? row : max), resultsOutput[0]);
+    const minPriceRowOutput = resultsOutput.reduce((min, row) => (row.price < min.price ? row : min), resultsOutput[0]);
+
+    const totalPriceOutput = resultsOutput.reduce((sum, row) => sum + row.price, 0);
+    const avgPriceOutput = totalPriceOutput / resultsOutput.length;
+
+    const totalProfit = totalPriceInput - totalPriceOutput;
 
     // Trả về kết quả dưới dạng JSON
     return res.status(200).json({
         success: true,
-        results,
+        resultsInput,
+        maxPriceRowInput,
+        minPriceRowInput,
+        avgPriceInput,
+        resultsOutput,
+        maxPriceRowOutput,
+        minPriceRowOutput,
+        avgPriceOutput,
+        totalPriceInput,
+        totalPriceOutput,
+        totalProfit,
     });
 });
 
