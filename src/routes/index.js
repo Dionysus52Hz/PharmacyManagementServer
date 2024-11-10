bd928fd7f7ebec959e277a3e8df4e27634194551
Khai Nguyen <khaib2111847@student.ctu.edu.vn>
2024-11-10 16:55:29 +0700

    add transaction to API medicine!

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,3 @@
-import userRouter from './UserRouter.js';
 import deliveryNoteRoutes from './DeliveryNoteRoutes.js';
 import medicineRoutes from './medicineRoutes.js';
 import receivedNoteRoutes from './receivedNoteRoutes.js';
@@ -7,7 +6,6 @@ import deliveryNoteDetailsRoutes from './deliveryNoteDetailsRoutes.js';
 
 
 const route = (app) => {
-    app.use('/api/user', userRouter);
     app.use('/api/delivery-notes', deliveryNoteRoutes);
     app.use('/api/medicines', medicineRoutes);
     app.use('/api/receivednotes', receivedNoteRoutes);
--- src/routes/medicineRoutes.js
+++ src/routes/medicineRoutes.js
@@ -6,9 +6,9 @@ import verifyToken from '../middlewares/verifyTokenMiddleware.js';
 const router = express.Router();
 
 router.get('/',medicineController.getMedicines);
-// router.get('/:medicine_id', verifyToken.verifyAccessToken, medicineController.getMedicineById);
-// router.post('/', verifyToken.verifyAccessToken, medicineController.createMedicine);
-// router.put('/:medicine_id', verifyToken.verifyAccessToken, medicineController.updateMedicine);
-// router.delete('/:medicine_id', verifyToken.verifyAccessToken, medicineController.deleteMedicine);
+router.get('/:medicine_id', medicineController.getMedicineById);
+router.post('/', medicineController.createMedicine);
+router.put('/:medicine_id', medicineController.updateMedicine);
+router.delete('/:medicine_id', medicineController.deleteMedicine);
 
 export default router;
